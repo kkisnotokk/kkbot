f92828b60377bad78b50e9e09ffc9ba47a551a96
kkisnotokk <kkalternate09@gmail.com>
2025-08-02 21:54:37 -0400

    Eightball update??

--- main.py
+++ main.py
@@ -223,16 +223,20 @@ async def pingroulette(ctx):
     await ctx.send(fake_ping)
 
 @bot.command(help="Ask the all-knowing 8-ball a question")
-async def eightball(ctx, *, question: str = None):
+async def 8ball(ctx, *, question: str = None):
     rigged = rigged_responses.pop(ctx.author.id, None)
     if rigged:
-        await ctx.send(f"🎯 {rigged} *(rigged)*")
+        await ctx.send(f"❌ {rigged}")
         return
     responses = [
         "Yes.", "No.", "Maybe.", "Absolutely.", "Absolutely not.",
-        "Ask again later.", "OUT OF ALL THE QUESTIONS YOU CAN ASK, YOU ASK THAT??",
+        "Ask again later.", "OUT OF ALL THE QUESTIONS YOU CAN ASK, YOU ASK THAT?",
         "Without a doubt.", "I wouldn’t count on it.",
-        "99% sure it’s yes.", "Try again after touching grass.", "You really don't want me to answer that", "Idk ask Emilybot"
+        "I'm tired of your dumb questions, go ask Emilybot.", "Try again after touching grass.", "You really don't want me to answer that",
+        "Idk ask Emilybot.", "All will be clear soon, wait, no it won't I lied you're cooked.",
+        "It's ggs {ctx.author.name}, you know the answer.", "Er uh you don't need to know that.",
+        "I am not answering that.", "Shut up I'm busy getting drunk.",
+        "@kk there's been an error, I think I ran out of shits to give."
     ]
 
     if not question:
