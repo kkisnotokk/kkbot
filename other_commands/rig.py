f0de0aea5b937e96a0949e5ec1f287b8c38f23dd
kkisnotokk <kkalternate09@gmail.com>
2025-08-04 13:40:29 -0400

    Chatgpt bug fix lmao

--- other_commands/rig.py
+++ other_commands/rig.py
@@ -1,30 +1,24 @@
-# other_commands/rig.py
-
 import discord
 from discord.ext import commands
 
-# This will be overwritten by main.py's version after import
 rigged_responses = {}
 
 class Rig(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.rigged_responses = rigged_responses
 
-    @commands.command(help="Rig the bot's next <roll, <eightball, or <rate response to say what you want.")
+    @commands.command(help="Rig the bot's next response to a command.")
     async def rig(self, ctx, *, message: str):
-        rigged_responses[ctx.author.id] = message
+        self.rigged_responses[ctx.author.id] = message
         await ctx.send(f":3 Your next command is rigged to say: `{message}`")
 
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        if ctx.author.id in rigged_responses:
-            rigged_msg = rigged_responses.pop(ctx.author.id)
+        if ctx.author.id in self.rigged_responses:
+            rigged_msg = self.rigged_responses.pop(ctx.author.id)
             await ctx.send(f"[RIGGED] {rigged_msg}")
             raise commands.CheckFailure("RIGGED: command execution halted.")
 
 def setup(bot):
-    # Assign the shared dictionary to the cog’s copy
-    from main import rigged_responses as shared_rigged
-    globals()["rigged_responses"] = shared_rigged
-
     bot.add_cog(Rig(bot))
